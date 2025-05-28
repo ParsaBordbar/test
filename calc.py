fe89e20f73ff1213708b61204e228f80bceca7f6
ParsaBordbar <parsab71@gmail.com>
2025-05-28 21:37:02 +0330

    refactor(calc.py): add find func - refactored find code into a function - it gets a list of numbers and prints found if the number is in the list

--- calc.py
+++ calc.py
@@ -3,14 +3,14 @@ y = 4
 
 numbers_list = [20, 12, -2, 88, 90, 200, -100, 20]
 
-for i in numbers_list: 
-    if (i == 90):
-        print("Found")
-    else: 
-        print("Not Found")
+def find(list):
+    for i in list: 
+        if (i == 90):
+            print("Found")
+        else: 
+            print("Not Found")
 
-# print(x - y)
-# print(x * y * 2)
+find(numbers_list)
 
 def sum(a, b):
     return a + b
