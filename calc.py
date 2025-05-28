a7ebe2f8a70e20ebdae563d88152e7c82a45d627
ParsaBordbar <parsab71@gmail.com>
2025-05-28 21:34:06 +0330

    feat(calc.py): code to find 90

--- calc.py
+++ calc.py
@@ -1,11 +1,17 @@
 x = 5 
 y = 4
 
-sum(x, 4)
+numbers_list = [20, 12, -2, 88, 90, 200, -100, 20]
 
-print(x - y)
-print(x * y * 2)
+for i in numbers_list: 
+    if (i == 90):
+        print("Found")
+    else: 
+        print("Not Found")
 
-def sum(a, b) {
+# print(x - y)
+# print(x * y * 2)
+
+def sum(a, b):
     return a + b
-}
+
